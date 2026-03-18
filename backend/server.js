9988f6a8aae5d9292d80d1d499a04109388283a2
Kaique <Kaiqueg6789@gmail.com>
2026-03-18 19:42:42 -0300

    configurar cors para producao

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: 'https://petgood-gamma.vercel.app'
